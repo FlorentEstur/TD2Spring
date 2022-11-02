79f186136ce4d833d80140ebf47acf46114c28f3
FlorentEstur <warcr@DESKTOP-3VUL9G4.home>
2022-11-02 16:57:31 +0100

    Ajout pour la fin de l'exercice 1

--- src/main/java/com/inti/controller/ProduitController.java
+++ src/main/java/com/inti/controller/ProduitController.java
@@ -1,9 +1,14 @@
 package com.inti.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.inti.model.Produit;
@@ -15,10 +20,24 @@ public class ProduitController {
 
 	@Autowired
 	IProduitRepository ipr;
-	
+
 	@GetMapping("/affichage")
-	public Produit produitById(@PathVariable int id)
-	{
-		return ipr.findById(id).get();
+	public String produitByNom(@RequestParam(name = "nom", defaultValue = "patate", required = true) String nom) {
+		return nom;
+	}
+
+	@GetMapping("/prix")
+	public Double produitByPrix(@RequestParam(name = "prix", defaultValue = "5", required = true) double prix) {
+		return prix;
+	}
+
+	@GetMapping("/calcul")
+	public List<String> prixProduitTTC(@RequestParam(name = "nom", defaultValue = "patate") String nom, @RequestParam (name ="prix", defaultValue = "5") double prix) {
+		List<String> listeprixTTC = new ArrayList<>();
+		double prixTTC = 1.8*prix;
+		String prixTTCString = String.valueOf(prixTTC);
+		listeprixTTC.add(prixTTCString);
+		return listeprixTTC;
+
 	}
 }
